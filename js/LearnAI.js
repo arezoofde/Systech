461f89f52eb676aec08bf7586b80e356afe06619
ArezooFadaei <arezoo.fde1999@gmail.com>
2024-01-29 00:07:01 +0330

    24/1/29 update

--- js/LearnAI.js
+++ js/LearnAI.js
@@ -1,37 +1,10 @@
+let currentText = null
 
-console.log('fieeeeeeerst')
-let cardboxSection1 = document.getElementById("section1");
-let cardboxSection2 = document.getElementById("section2");
-let cardboxSection3 = document.getElementById("section3");
-let cardboxSection4 = document.getElementById("section4");
-let boxText = document.querySelectorAll("box-text");
+function showText(buttonID) {
+  if (currentText !== null) {
+    currentText.style.display = 'none'
+  }
+  currentText = document.getElementById('text' + buttonID[buttonID.length -1]) 
+  currentText.style.display = 'block'
 
-
-function showText(boxTextId) {
-  // یافتن تمام المان‌های boxText و پنهان کردن آن‌ها
-  let allBoxsText = document.querySelectorAll('.box-text');
-  allBoxsText.forEach(function(boxText) {
-    boxText.classList.add('hidden');
-    boxText.classList.remove('visible');
-  });
-
-  boxText.classList.remove('hidden');
-  boxText.classList.add('visible');
 }
-cardboxSection1.addEventListener("click", function () {
-  showText(boxText);
-  
-});
-
-
-cardboxSection2.addEventListener("click", function () {
-  showText('boxText2');
-});
-
-cardboxSection3.addEventListener("click", function () {
-  showText('boxText3');
-});
-
-cardboxSection4.addEventListener("click", function () {
-  showText('boxText4');
-});
